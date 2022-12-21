070d518b736de22b1f9a49c853c7a4700d6e932a
Anes Hrvačić <hrvacicanes@gmail.com>
2022-12-21 19:03:12 +0100

    Added bar chart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./scenes/dashboard/Dashboard";
 import Team from "./scenes/team/Team";
 import Invoices from "./scenes/invoices/Invoices";
 import Contacts from "./scenes/contacts/Contacts";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar/Bar";
 import Form from "./scenes/form/Form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
@@ -32,8 +32,8 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              {/* <Route path="/bar" element={<Bar />} />
-							<Route path="/pie" element={<Pie />} />
+              <Route path="/bar" element={<Bar />} />
+              {/* <Route path="/pie" element={<Pie />} />
 							<Route path="/line" element={<Line />} />
               <Route path="/geography" element={<Geography />} />*/}
               <Route path="/faq" element={<FAQ />} />
